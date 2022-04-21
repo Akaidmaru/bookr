2fbf7badc42553ab076a109c9df48a0bef02a304
Akaidmaru <reddmar.quevedo@gmail.com>
2022-04-20 23:36:54 -0500

    refactor: moved url configurations.
    I created a new file: reviews/urls.py, it will have all the urls related to reviews app.
    
    Also, a modification has been made to bookr/urls.py so it can include the just created reviews/urls.py and Django could access it.
    
    This refactor helps to manage urls in an easier way.

--- bookr/urls.py
+++ bookr/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 import reviews.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', reviews.views.index),
-    path('book-search', reviews.views.book_search)
+    path('', include('reviews.urls')),
+    # path('', reviews.views.index),
+    # path('book-search', reviews.views.book_search)
 ]
