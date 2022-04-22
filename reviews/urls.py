ec4792357eec9912db6954bd765503a5a1e7fd40
Akaidmaru <reddmar.quevedo@gmail.com>
2022-04-21 23:25:36 -0500

    feat: book_list urls
    I added book_list url.

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    path('', views.welcome_view, name='welcome-view'),
+    path('books/', views.book_list, name='book_list'),
                ]
