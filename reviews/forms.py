c310895fc6f8cb9ba187a5dbbc7949dfad4aa3c4
Akaidmaru <reddmar.quevedo@gmail.com>
2022-05-13 15:54:49 -0500

    feat: SearchForm
    I created a new SearchForm class in a new forms.py file, so it can get some information to make a query, i need to keep working on the view.

---
+++ reviews/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+SEARCH_CHOICES = (
+    ("title", "Title"),
+    ("contributor", "Contributor")
+    )
+
+
+class SearchForm(forms.Form):
+    """Basic Search Form, it will work with a minimum of 3 characters, selecting if you want to search
+    by Title or Contributor, and neither of them is required for the form to work. By default, search is set
+    to search_in by Title."""
+    search = forms.CharField(min_length=3, required=False)
+    search_in = forms.ChoiceField(choices=SEARCH_CHOICES, default='title', required=False)
