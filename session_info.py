071a3ee5f2c05f2faeb701935e25e48b1f9ac5cc
Reddmar Quevedo <reddmar.quevedo@gmail.com>
2022-06-11 01:31:04 -0500

    chore: script creation to check any session you want on django
    - Creation of a Python Script to check a Django session without having to use the python manage.py shell command, you only need to use python session_info.py <index of the required session>, the index starts on 0,.
    
    This session index will allow the script to search that session key and will allow SessionStore to decode it, returning a dictionary with the current session_dictionary information.
    
    This script is helpful to debug storing info in sessions.

---
+++ session_info.py
@@ -0,0 +1,25 @@
+import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookr.settings')
+django.setup()
+
+
+from django.contrib.sessions.backends.db import SessionStore
+from django.contrib.sessions.models import Session
+import sys
+import pprint
+
+
+def get_session_dictionary(session_index):
+    session_store = SessionStore()
+    sessions = Session.objects.all()
+    session_dictionary = session_store.decode(sessions[int(session_index)].session_data)
+    return session_dictionary
+
+
+if __name__ == '__main__':
+    if len(sys.argv)>1:
+        session_index = sys.argv[1]
+        session_dictionary = get_session_dictionary(session_index)
+        pp = pprint.PrettyPrinter(indent=4)
+        pp.pprint(session_dictionary)
