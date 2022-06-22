d8c22dfef0e604e0af1430de78870393a83758bc
Reddmar Quevedo <reddmar.quevedo@gmail.com>
2022-06-21 23:27:33 -0500

    feat: session search_history
    Created the logic so the search_history is saved for each user.

--- reviews/views.py
+++ reviews/views.py
@@ -20,11 +20,14 @@ def index(request):
 
 def book_search(request):
     search_text = request.GET.get("search", "")
+    search_history = request.session.get('search_history', [])
     form = SearchForm(request.GET)
     books = set()
+
     if form.is_valid() and form.cleaned_data['search']:
         search = form.cleaned_data["search"]
         search_in = form.cleaned_data.get("search_in") or "title"
+
         if search_in == "title":
             books = Book.objects.filter(title__icontains=search)
         else:
@@ -40,7 +43,15 @@ def book_search(request):
                 for book in contributor.book_set.all():
                     books.add(book)
 
-    return render(request, "search_result.html", {"form": form, "search_text": search_text, "books": books})
+        if request.user.is_authenticated:
+            search_history.append([search_in, search])
+            request.session['search_history'] = search_history
+
+    elif search_history:
+        initial = dict(search=search_text, search_in=search_history[-1][0])
+        form = SearchForm(initial=initial)
+
+    return render(request, "reviews/search_result.html", {"form": form, "search_text": search_text, "books": books})
 
 
 def book_list(request):
